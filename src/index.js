f4d87469ea00f046319f712285dd38b79a8b9c24
RohitChiki <rohitsinghwd1993@gmail.com>
2019-12-25 20:36:13 +0530

    initial sticky notes app done.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+const firebase = require('firebase');
+require('firebase/firestore');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+firebase.initializeApp({/*firebase credentials goes here.*/})
+
+ReactDOM.render(<App />, document.getElementById('sticky-container'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
